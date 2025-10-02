1471f1d78265974d975e9487dc3bca55fa5dbbd7
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-02 14:00:59 +0600

    tailwind added with v3

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./**/templates/**/*.{html,js}", //apps ar vhitor
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
