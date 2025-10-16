4aa7cd8b0a1ef701496babe103e27a0f974ce0f7
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-16 10:33:05 +0600

    data show from db

--- Shoe/views.py
+++ Shoe/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from Shoe.form import Add_Shoe_Form
+from Shoe.models import Shoe
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    x = Shoe.objects.all()
+    
+    return render(request, 'home.html', {"x" : x})
 
 def Add_Product(request):
     form = Add_Shoe_Form()
