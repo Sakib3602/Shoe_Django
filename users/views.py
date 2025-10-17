7c23e186c5caa4924df847703ba2de3adf96886b
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-17 15:44:56 +0600

    sign up user done

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 def SignUp(request):
+    if request.method == "POST":  
+        print(request.POST)
+        name  = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("Password")
+        em = User.objects.filter(email=email).exists()
+        if len(password) > 6 and em == False:
+            print(name,email,password)
+            user = User.objects.create_user(name, email, password)
+            user.save()
+            return redirect( "signin")
+    
     return render(request, "reg.html")
 
 def SignIn(request):
