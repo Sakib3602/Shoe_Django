32f58c9faee406028266590161734c84885fc3c7
Sakib3602 <sakibsarker6969@gmail.com>
2025-10-17 16:06:10 +0600

    user.is_authenticate use and daynamic button added

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def SignUp(request):
     if request.method == "POST":  
@@ -17,4 +18,13 @@ def SignUp(request):
     return render(request, "reg.html")
 
 def SignIn(request):
+    print(request.POST)
+    if request.method == "POST":
+        name = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=name, password=password)
+        if user is not None:
+            login(request, user)
+            print("Log in Successful")
+            return redirect("/")
     return render(request, "log.html")
